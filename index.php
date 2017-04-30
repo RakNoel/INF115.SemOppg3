1fcb492e4371c1cc7e96c09a8c8e4f5740782c93
Oskar Leonhard Fretheim Leirvåg <oskar.leirvag@student.uib.no>
2017-04-30 21:42:38 +0200

    Finished index
    > Added fully automated menu creator
    > Added fully functional index
    >> Index now loads task functions form task files

--- index.php
+++ index.php
@@ -1,11 +1,35 @@
 <!DOCTYPE>
 <HTML>
-	<HEAD>
-        <Title>OLE006 - Løsning</Title>
-        <link rel="stylesheet" href="css/default.css">
-    </HEAD>
+<HEAD>
+    <Title>OLE006 - Løsning</Title>
+    <link rel="stylesheet" href="resources/css/default.css">
+    <link rel="stylesheet" href="resources/css/menu.css">
+    <link rel="stylesheet" href="resources/css/table.css">
+</HEAD>
 
-	<BODY>
+<BODY>
 
-    </BODY>
+<?php
+include 'menu.php';
+printMenu();
+?>
+
+<div id="main">
+
+    <?php
+    if (isset($_REQUEST['task']) && is_numeric($_REQUEST['task']))
+        if (file_exists("Oppgave" . $_REQUEST['task'] . ".php")){
+            include "Oppgave" . $_REQUEST['task'] . ".php";
+            printContent();
+        } else {
+            http_redirect();
+        }
+    else{
+        echo "Velkommen til OLE006 sitt løsningsforslag av den obligatoriske oppgave nr 3";
+    }
+    ?>
+
+</div>
+
+</BODY>
 </HTML>
---
+++ menu.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: oskar
+ * Date: 30.04.2017
+ * Time: 20.30
+ */
+
+
+/**
+ * PrintMenu er en funksjon som printer ut all HTML
+ * kode for å få printe ut menyen korrekt.
+ */
+function printMenu()
+{
+    echo "<div id='primary_nav_wrap'>";
+    echo "<ul><li><a href='index.php'><span>Hjem</span></a></li>";
+    echo "<li><a><span>Oppgaver</span></a>";
+    echo "<ul>";
+
+    $dir = '.';
+    $oppgaveListe = scandir($dir);
+
+    if ($oppgaveListe != false)
+        foreach ($oppgaveListe as $value)
+            if (substr($value, 0, 1) == "O")
+                echo "<li><a href='index.php?task="
+                    . (substr($value, 7, 1))
+                    . "'><span>" . "$value" . "</span></a></li>";
+
+    echo "</ul></li></ul></div>";
+}
