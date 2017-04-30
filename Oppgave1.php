b4e4e7d3cc87427133faeac7f0444d6fa37f2ffd
Oskar Leonhard Fretheim Leirvåg <oskar.leirvag@student.uib.no>
2017-04-30 21:43:00 +0200

    Finished task 1
    started on task 2

---
+++ Oppgave1.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oskar
+ * Date: 30.04.2017
+ * Time: 21.16
+ */
+
+function printContent()
+{
+    echo "Good luck with the last compulsory!";
+}
---
+++ Oppgave2.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oskar
+ * Date: 30.04.2017
+ * Time: 21.40
+ */
